d919765667545fd5de9c23ae1f59edbd4017e95b
Boris Adamyan <boris.adamyan@gmail.com>
2018-08-07 15:01:01 +0400

    formated time with moment js

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+const moment = require('moment');
+
+
+/*
+ var date = moment();
+date.add(1,'year').subtract(9, 'months');
+
+console.log(date.format('MMM Do YYYY'));
+
+*/
+
+/*console.log(date.format('h:mm a'))*/
+
+var someTime = moment().valueOf();
+console.log(someTime);
+
+var date = moment(someTime);
+console.log(date.format('h:mm a'))
